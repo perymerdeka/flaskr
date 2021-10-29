abf7e6b610dd5c7a0d7c957238394ab9eef20c50
perymerdeka <tkjasikk@gmail.com>
2021-10-29 21:37:42 +0700

    Add Makefile and WIP explain session client side concept

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+
+app.secret_key = "This Secret Key"
 
 # route
 @app.route("/")
@@ -15,3 +17,25 @@ def index():
 @app.route("/profile/<username>")
 def profile(username: str):
     return render_template("profile.html", username=username)
+
+
+@app.route("/set_email", methods=["GET", "POST"])
+def set_email():
+    if request.method == "POST":
+        # save session
+        session["email"] = request.form["email_address"]
+    return render_template("set_email.html")
+
+
+# get session email
+@app.route("/get_email")
+def get_email():
+    return render_template("get_email.html")
+
+
+# delete session email
+@app.route("/delete_email")
+def delete_email():
+    # Clear the email stored in the session object
+    session.pop("email", default=None)
+    return "<h1>Session deleted!</h1>"
